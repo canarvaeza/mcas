e0c0008537630d22551e82041afd4f6d30c3e80f
Cristian Narvaez <cristian.narvaez@globant.com>
2018-12-14 15:47:25 -0500

    minor fix

--- Apps/mcas/mcas/src/main/java/mcas/Controller/MainController.java
+++ Apps/mcas/mcas/src/main/java/mcas/Controller/MainController.java
@@ -33,45 +33,51 @@ public class MainController {
 	}
 
 	public static void main(String[] args) throws Exception {
-//		Logger logg_one_insert = newLogger("one_insert_logger");
+		// Logger logg_one_insert = newLogger("one_insert_logger");
 		Logger logg_multiple_insert = newLogger("multiple_insert_logger");
 
 		VirtGraph graph = new VirtGraph(QueryConf.connectionString, QueryConf.userName, QueryConf.passWord);
 
-		String[] files = { "Sensors.json", "Locations.json", "Persons.json", "Multimedia.json", "test0.json", "test1.json", "test2.json", "test3.json", "test4.json", "test5.json", "test6.json","test7.json", "test8.json", "test9.json", "test10.json", "test11.json", "test12.json" }; // long
-//		String[] files = {"Multimedia.json","test0.json", "test1.json", "test2.json",	"test3.json", "test4.json", "test5.json", "test6.json","test7.json", "test8.json", "test9.json", "test10.json", "test11.json", "test12.json"}; // long
-//		String[] files = { "Sensors.json", "Locations.json", "Persons.json", "Multimedia.json", "test10.json","test11.json"};
+		String[] files = { "Sensors.json", "Locations.json", "Persons.json", "Multimedia.json", "test0.json",
+				"test1.json", "test2.json", "test3.json", "test4.json", "test5.json", "test6.json", "test7.json",
+				"test8.json", "test9.json", "test10.json", "test11.json", "test12.json" }; // long
+		// String[] files = {"Multimedia.json","test0.json", "test1.json", "test2.json",
+		// "test3.json", "test4.json", "test5.json", "test6.json","test7.json",
+		// "test8.json", "test9.json", "test10.json", "test11.json", "test12.json"}; //
+		// long
+		// String[] files = { "Sensors.json", "Locations.json", "Persons.json",
+		// "Multimedia.json", "test10.json","test11.json"};
 
 		Initializer initializer = new Initializer();
 		initializer.setNewDataFiles(files);
-		initializer.initialize(false, false, graph, TEST_RESOURCES_PATH);
-		
+		initializer.initialize(false, false, false, graph, TEST_RESOURCES_PATH);
+
 		logg_multiple_insert.info("create multiple 100 times 700 data each");
 		Instant start = Instant.now();
 		for (int j = 0; j < 1; j++) {
 			for (int i = 0; i < 1; i++) {
-				initializer.initialize(true, true, graph, TEST_RESOURCES_PATH);
+				initializer.initialize(true, true, false, graph, TEST_RESOURCES_PATH);
 			}
 		}
 		Instant finish = Instant.now();
 		long timeElapsed = Duration.between(start, finish).toNanos();
 		logg_multiple_insert.info(Long.toString(timeElapsed));
 
-//		initializer.initialize(false, true, graph, TEST_RESOURCES_PATH);
+		// initializer.initialize(false, true, graph, TEST_RESOURCES_PATH);
 
 		// String rdfFlux = "";
 
 		// generate random UUIDs
-//		UUID idOne = UUID.randomUUID();
-//		UUID idTwo = UUID.randomUUID();
-//		System.out.println("UUID One: " + idOne);
-//		System.out.println("UUID Two: " + idTwo);
-//
-//		String date = "2018-10-06";
-//		List<String> rules = KGraphManager.getRules(graph, date);
-//		String date_before = DatesManager.getNewDate(date, -1);
-////		System.out.println(rules);
-//		Queries.constructNewRule(graph, "", rules, date, date_before);
+		// UUID idOne = UUID.randomUUID();
+		// UUID idTwo = UUID.randomUUID();
+		// System.out.println("UUID One: " + idOne);
+		// System.out.println("UUID Two: " + idTwo);
+		//
+		// String date = "2018-10-06";
+		// List<String> rules = KGraphManager.getRules(graph, date);
+		// String date_before = DatesManager.getNewDate(date, -1);
+		//// System.out.println(rules);
+		// Queries.constructNewRule(graph, "", rules, date, date_before);
 
 	}
 }
